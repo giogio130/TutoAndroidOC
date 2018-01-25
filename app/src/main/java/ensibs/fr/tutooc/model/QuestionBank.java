2012fa2787746c6a9f163f7ac308ca17d26bd37e
giogio130 <giovannigwada@gmail.com>
2018-01-25 14:13:14 +0100

    Fin tuto

---
+++ app/src/main/java/ensibs/fr/tutooc/model/QuestionBank.java
@@ -0,0 +1,32 @@
+package ensibs.fr.tutooc.model;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Giovanni MIRTIL on 25/01/2018.
+ */
+
+public class QuestionBank {
+    private List<Question> mQuestionList;
+    private int mNextQuestionIndex;
+
+    public QuestionBank(List<Question> questionList) {
+        mQuestionList = questionList;
+
+        // Shuffle the question list
+        Collections.shuffle(mQuestionList);
+
+        mNextQuestionIndex = 0;
+    }
+
+    public Question getQuestion() {
+        // Ensure we loop over the questions
+        if (mNextQuestionIndex == mQuestionList.size()) {
+            mNextQuestionIndex = 0;
+        }
+
+        // Please note the post-incrementation
+        return mQuestionList.get(mNextQuestionIndex++);
+    }
+}
---
+++ app/src/main/java/ensibs/fr/tutooc/model/User.java
@@ -0,0 +1,24 @@
+package ensibs.fr.tutooc.model;
+
+/**
+ * Created by Giovanni MIRTIL on 25/01/2018.
+ */
+
+public class User {
+    private String mFirstName;
+
+    public String getFirstName() {
+        return mFirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        mFirstName = firstName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "mFirstName='" + mFirstName + '\'' +
+                '}';
+    }
+}
